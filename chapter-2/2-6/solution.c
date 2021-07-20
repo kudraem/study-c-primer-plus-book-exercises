5c6b18391887fb0bcb911d8c6d1f34a25f8b8bbf
Ivan Khimich <himivan@gmail.com>
2021-07-21 00:33:02 +0300

    Added chapter 2 exercise 6 solution

---
+++ chapter-2/2-6/solution.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int toes;
+	toes = 10;
+
+	printf("Значение toes - %d\n", toes);
+	printf("Удвоенное значение toes - %d\n", toes * 2);
+	printf("Квадрат значения toes - %d\n", toes * toes);
+
+	return 0;
+}
